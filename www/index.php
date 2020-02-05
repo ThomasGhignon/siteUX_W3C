edcd6994eb3ad156dbf056770a18604ee16f71c1
ThomasGhignon <ghignon.thomas@gmail.com>
2020-02-05 16:27:36 +0100

    set

---
+++ www/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width"/>
+  <meta name="keywords" content="HTML,CSS,JavaScript,PHP">
+  <meta name="author" content="Thomas Ghignon">
+  <title>Document</title>
+
+  <link rel="stylesheet" href="css/libs/cssReset.css">
+  <link rel="stylesheet" href="css/style_index.css">
+  <link rel="stylesheet" href="css/style_global.css">
+</head>
+<body>
+
+  <header>
+    <div class="wrap">
+
+    </div>
+  </header>
+
+  <script src="js/libs/jquery.js"></script>
+</body>
+</html>
